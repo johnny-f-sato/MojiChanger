d78cd9b57a16dee7a830b943132717a095565457
johnny-f-sato <f.sato@ysinc.co.jp>
2016-05-24 21:36:04 +0900

    add backend entities

---
+++ src/jp/co/ysinc/mojichange/application/Juror.java
@@ -0,0 +1,13 @@
+package jp.co.ysinc.mojichange.application;
+
+/**
+ * Created by Fumiya on 2016/05/24.
+ */
+public class Juror {
+
+    //正誤判定
+    public boolean judgeRightOrWrong(String input, String correct) {
+        return correct.equals(input);
+    }
+
+}
---
+++ src/jp/co/ysinc/mojichange/application/Stagehand.java
@@ -0,0 +1,8 @@
+package jp.co.ysinc.mojichange.application;
+
+/**
+ * Created by Fumiya on 2016/05/24.
+ */
+public abstract class Stagehand extends Thread {
+
+}
