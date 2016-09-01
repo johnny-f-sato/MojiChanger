cbfa2b0cbdf273e3c94d226b459b2437057a45d0
fumiya sato <f.sato@ysinc.co.jp>
2016-09-01 23:25:31 +0900

    add timer

--- src/jp/co/ysinc/mojichange/domain/entity/Timer.java
+++ src/jp/co/ysinc/mojichange/domain/entity/Timer.java
@@ -4,4 +4,10 @@ package jp.co.ysinc.mojichange.domain.entity;
  * Created by Fumiya on 2016/04/02.
  */
 public class Timer {
+
+    public static Timer newInstace(int duration) {
+        Timer timer = new Timer();
+
+    }
+
 }
